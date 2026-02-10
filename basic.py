cb8a9de65e8034180234a2a4238c3d8e8ee4d1a9
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2026-02-10 21:45:10 +0530

    Add basic Python type and operation examples

---
+++ basic.py
@@ -0,0 +1,38 @@
+a=2
+print(type(a))
+b=2.3
+print(type(b))
+c=2+3j
+print(type(c))
+d='a'
+print(type(d))
+e=None
+print(type(e))
+f=True
+print(type(f))
+
+# Operations 
+a=23
+b=24
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# conditional 
+i = 99
+
+if i <= 0:
+    print("please enter a positive number")
+elif i >= 90:
+    print("1st")
+elif i >= 80:
+    print("2nd")
+elif i >= 60:
+    print("3rd")
+elif i >= 35:
+    print("pass")
+else:
+    print("Fail")
