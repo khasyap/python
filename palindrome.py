dcbff4a81b72fed45761873bcdf28bd8e7e3a870
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2026-02-14 20:13:10 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -4,3 +4,12 @@ if str(num) == str(num)[::-1]:
     print("Palindrome")
 else:
     print("Not palindrome")
+
+
+print("----------------String checking palindrome or not ------------------------------")
+text = "madam"
+
+if text == text[::-1]:
+    print("Palindrome")
+else:
+    print("Not palindrome")
