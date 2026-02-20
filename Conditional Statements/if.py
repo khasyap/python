b19d9b9a2999d491f5f62ca5d5dbd4e0f7b1c7e7
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2026-02-20 23:15:40 +0530

    Update if.py

--- Conditional Statements/if.py
+++ Conditional Statements/if.py
@@ -1,1 +1,14 @@
+i = int(input())
 
+if i <= 0:
+    print("please enter a positive number")
+elif i >= 90:
+    print("1st")
+elif i >= 80:
+    print("2nd")
+elif i >= 60:
+    print("3rd")
+elif i >= 35:
+    print("pass")
+else:
+    print("Fail")
