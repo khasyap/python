56b1c5e3fbba42a74834f2324818effc5b26ac85
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2026-03-10 22:37:30 +0530

    Add files via upload

---
+++ Module/arithmetic.py
@@ -0,0 +1,10 @@
+from cal import *
+a=int(input())
+b=int(input())
+
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+print(mod(a,b))
+print(rem(a,b))
---
+++ Module/demo1.py
@@ -0,0 +1,11 @@
+def add():
+    print("Result 1 is ",__name__)
+def sub():
+    print("Result 2 is ",__name__)
+
+def main():
+    add()
+    sub()
+
+if __name__=="__main__":
+    main()
