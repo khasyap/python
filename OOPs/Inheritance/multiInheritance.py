ed370d30519909ae23b5eed743d90508681576d3
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2026-03-13 22:51:02 +0530

    Create multiInheritance.py

---
+++ OOPs/Inheritance/multiInheritance.py
@@ -0,0 +1,32 @@
+class A:
+    def feature1(self):
+        print("feature 1 is working")
+    def feature2(self):
+        print("feature 2 is working")
+class B(A):
+    def feature3(self):
+        print("feature 3 is working")
+    def feature4(self):
+        print("feature 4 is working")
+class C(B):
+    def feature5(self):
+        print("feature 5 is working")
+    def feature6(self):
+        print("feature 6 is working")
+
+a1=A()
+a1.feature1()
+a1.feature2()
+b1=B()
+b1.feature1()
+b1.feature2()
+b1.feature3()
+b1.feature4()
+
+c1=C()
+c1.feature1()
+c1.feature2()
+c1.feature3()
+c1.feature4()
+c1.feature5()
+c1.feature6()
